1b373fe0695b70d93342a082b14cbf22303b574e
walaamohammad <layan_abadi86@yahoo.com>
2021-08-17 09:14:13 +0300

    add

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -4,9 +4,9 @@ const userModel = require("../models/user.model");
 
 const booksController=(req,res)=>{
     const searchQuery=req.searchQuery.email;
-    userModel.findOne({email:searchQuery},(err ,user)=>{
-        if(err){
-            res.send(err.msg)
+    userModel.findOne({email:searchQuery},(user)=>{
+        if(!user){
+            res.send('user not found')
 
         }
         else{
